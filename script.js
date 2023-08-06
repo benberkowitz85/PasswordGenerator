982d4dba10ad6f7a3aa7acfcc2615fe78050ea4e
benberkowitz85 <139720587+benberkowitz85@users.noreply.github.com>
2023-08-06 17:24:13 -0400

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const pass_el = document.querySelector('#password span')
+const length: 16
+const characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
+
+function GeneratePassword () {
+    let pass = "";
+    for (var i = 0; 1 <= length; i++) {
+        let rand =math.floor(
+            math.random() *characters.length
+        );
+        pass+= characters.substring(rand, rand+1);
+    }
+    pass_el.innertext = pass;
+}
+
+GeneratePassword ()
