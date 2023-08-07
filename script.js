5a3677018bea6a97a51820d9a6a6d1a793f770a0
benberkowitz85 <139720587+benberkowitz85@users.noreply.github.com>
2023-08-06 20:12:37 -0400

    commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const generateOTP = () => {
 
     //loop throught the length of the OTP and add
     //random number between 0 and 9 to OTP string
-    for(let i = 0; 1 < length; i++) {
+    for(let i = 0; i < length; i++) {
         otp += Math.floor(Math.random() *10);
     }
     return otp;
